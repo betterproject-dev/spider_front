e202df9d836ae86ad940e55c9d22b834bb71ff63
altjs3310 <altjs3310@naver.com>
2025-12-24 14:44:47 +0900

    Feat:camera 구현 완료

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,10 +1,14 @@
-import { Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import Admin from "./features/Admin/pages/Admin"
+import Camera from "./features/Camera/pages/Camera"
+
 
 const Routers = () => {
   return (
     <>
       <Routes>
-        
+        <Route path="/" element={<Admin/>} />
+        <Route path="/camera" element={<Camera />} />
       </Routes>
     </>
   )
