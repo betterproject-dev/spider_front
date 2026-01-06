990cf4dc02d9c78fe448d8995c6c7c5e349003c3
jjop2 <jngsuh616@gmail.com>
2026-01-06 15:45:51 +0900

    fix: sensorConfig key 값 수정 반영

--- src/features/DashboardMachine/components/CurrentSensors.jsx
+++ src/features/DashboardMachine/components/CurrentSensors.jsx
@@ -8,7 +8,7 @@ const CurrentSensors = ({ realTimeData, selectedMachine }) => {
     
   // (임시) 더미데이터
   const testData = {
-    temperature : 21.62,
+    temperature_DS18B20 : 21.62,
     humidity : 9,
     noise : 59.06,
     leak : 1
@@ -23,7 +23,7 @@ const CurrentSensors = ({ realTimeData, selectedMachine }) => {
     const isNormal = checkIsNormal(sensor, data);
 
     return (
-      <div className="current_sensor_box" key={sensor.id} onClick={() => goTo(`/machine/${selectedMachine}/sensor/${sensor.key}`)} >
+      <div className="current_sensor_box" key={sensor.id} onClick={() => goTo(`/machine/${selectedMachine}/sensor/${sensor.eng_name}`)} >
         <div className="sensor_name_area">
           <div className="sensor_name">{sensor.name}</div>
         </div>
