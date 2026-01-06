6a4a775b2e46bea822ec39659b06bc276dc036a3
jjop2 <jngsuh616@gmail.com>
2026-01-06 10:46:33 +0900

    refact: 사용하지 않는 route 삭제

--- src/Route.jsx
+++ src/Route.jsx
@@ -3,8 +3,6 @@ import MonitoringMain from "./features/MonitoringMain/pages/MonitoringMain.jsx";
 import DashboardMachine from "./features/DashboardMachine/pages/DashboardMachine.jsx";
 import Admin from "./features/Admin/pages/Admin"
 import ProductChart from "./features/Chart/pages/ProductChart"
-import MachineDetail from "./features/SensorDetail/pages/MachineDetail";
-import Camera from "./features/DashboardMachine/components/Camera.jsx"
 import SensorDetailPage from "./features/SensorDetail/pages/SensorDetailPage.jsx";
 import DefectItemPage from "./features/DefectItem/pages/DefectItemPage.jsx";
 
@@ -16,10 +14,7 @@ const Routers = ({ realTimeData }) => {
         <Route path="/" element={<Admin/>} />
         <Route path="/monitor" element={<MonitoringMain />} />
         <Route path="/dashboard/:machineNum" element={<DashboardMachine realTimeData={realTimeData} />} />
-        {/* <Route path="/camera" element={<Camera />} /> */}
         <Route path="/chart" element={<ProductChart />} />
-        {/* <Route path="/machineDetail" element={<MachineDetail />} /> */}
-        {/* <Route path="/sensor" element={<SensorDetailPage realTimeData={realTimeData} />} /> */}
         <Route path="/machine/:machineNum/sensor/:sensorKey" element={<SensorDetailPage realTimeData={realTimeData} />} />
         <Route path="/machine/:machineNum/items/defect" element={<DefectItemPage />} />
       </Routes>
