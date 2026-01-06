184593743ca607bcd11022b51e21668f598e5790
unknown <thduszbzb12@gmail.com>
2026-01-06 12:39:01 +0900

    Fix: 기기 센서 spring으로 변경

--- src/features/DashboardMachine/pages/DashboardMachine.jsx
+++ src/features/DashboardMachine/pages/DashboardMachine.jsx
@@ -7,6 +7,7 @@ import Camera from "../components/Camera";
 import Loading from "../../../components/Loading/Loading";
 import CurrentSensors from "../components/CurrentSensors";
 import UseNavi from "../../../hooks/UseNavi.jsx";
+import requestHandler from "../../../utils/requestHandler.js";
 
 // 메시지 슬라이더용 데이터 (MonitoringMain에서 복사)
 const MESSAGE_ROW_HEIGHT = 35;
@@ -28,7 +29,8 @@ const DashboardMachine = ({ realTimeData }) => {
   const [transitionOn, setTransitionOn] = useState(true);
 
   // 작동 여부 (임시 : realTimeData에 값이 들어있는지 여부 -> 추후에 소켓 작동 여부로 변경)
-  const isWorking = realTimeData.length !== 0;
+  // spring에서 센서의 작동 여부를 가져오는 걸로 변경
+  const [isWorking, setIsWorking] = useState(false);
 
   useEffect(() => {
     const timer = setInterval(() => setCurrentTime(new Date()), 1000);
@@ -50,6 +52,24 @@ const DashboardMachine = ({ realTimeData }) => {
       setTimeout(() => setTransitionOn(true), 50);
     }
   };
+
+  useEffect(() => {
+    const fetchHeartbeatStatus = async () => {
+      const { ok, data } = await requestHandler({
+        method: "get",
+        url: `/api/heartbeat/status/${selectedMachine}`,
+        server: "spring",
+        onError: (msg) => console.error(msg)
+      });
+
+      if (ok) setIsWorking(data.status === "ONLINE");
+      else setIsWorking(false);
+    };
+
+    fetchHeartbeatStatus();
+    const interval = setInterval(fetchHeartbeatStatus, 5000);
+    return () => clearInterval(interval);
+  }, [selectedMachine]);
 
   return (
     <>
